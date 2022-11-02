fbd1bb30f37b4c2ecfcb11793e7dc1e247b1f741
Hasegawa-Akito <h.akito1999@gmail.com>
2022-11-02 16:35:45 +0900

    パス設定

--- src/components/Home/SelectStartType.tsx
+++ src/components/Home/SelectStartType.tsx
@@ -2,13 +2,13 @@ const SelectStartType = () => {
   return (
     <div>
       <a
-        href="#"
+        href="/create"
         className="inline-flex items-center px-8 py-3 text-white transition bg-gray-900 rounded-full shadow-lg focus:outline-none focus:ring focus:bg-teal-600 hover:bg-gray-800"
       >
         <span className="text-sm font-medium"> 自己紹介作成 →</span>
       </a>
       <a
-        href="#"
+        href="/show"
         className="ml-3 inline-flex items-center px-8 py-3 text-white transition bg-gray-900 rounded-full shadow-lg focus:outline-none focus:ring focus:bg-teal-600 hover:bg-gray-800"
       >
         <span className="text-sm font-medium"> カード取得 → </span>
