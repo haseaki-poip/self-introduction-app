6d3eb2957b44336d511667a3083214f96b3bc0a7
Hasegawa-Akito <h.akito1999@gmail.com>
2022-11-02 16:33:46 +0900

    位置情報取得までのローディングを表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import StartButtons from "../src/components/StartButtons";
+import StartButtons from "../src/components/Home/StartButtons";
 
 const Home: NextPage = () => {
   return (
---
+++ src/components/Home/SelectStartType.tsx
@@ -0,0 +1,20 @@
+const SelectStartType = () => {
+  return (
+    <div>
+      <a
+        href="#"
+        className="inline-flex items-center px-8 py-3 text-white transition bg-gray-900 rounded-full shadow-lg focus:outline-none focus:ring focus:bg-teal-600 hover:bg-gray-800"
+      >
+        <span className="text-sm font-medium"> 自己紹介作成 →</span>
+      </a>
+      <a
+        href="#"
+        className="ml-3 inline-flex items-center px-8 py-3 text-white transition bg-gray-900 rounded-full shadow-lg focus:outline-none focus:ring focus:bg-teal-600 hover:bg-gray-800"
+      >
+        <span className="text-sm font-medium"> カード取得 → </span>
+      </a>
+    </div>
+  );
+};
+
+export default SelectStartType;
