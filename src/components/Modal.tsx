2d08b8f03bb410046684d8ab960f3bc7f00bc0af
Hasegawa-Akito <h.akito1999@gmail.com>
2022-11-09 23:36:21 +0900

    component構成変更

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,27 +14,7 @@ const Modal = ({ children, closeModal }: PropsType) => {
         className="absolute bg-black opacity-20 inset-0 z-0"
         onClick={() => closeModal()}
       ></div>
-      <div className="sticky">
-        <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-28 lg:my-0">
-          {children}
-          <span
-            className="mx-auto my-6 lg:mx-0 flex justify-center items-center p-6 cursor-pointer rounded-full bg-gray-600 lg:rounded-none lg:rounded-r-full transform duration-500 hover:scale-125"
-            onClick={() => closeModal()}
-          >
-            <svg
-              version="1.1"
-              id="_x32_"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              className="h-4 fill-current text-white"
-            >
-              <g>
-                <polygon points={iconPathList.cross}></polygon>
-              </g>
-            </svg>
-          </span>
-        </div>
-      </div>
+      <div className="sticky">{children}</div>
     </div>
   );
 };
